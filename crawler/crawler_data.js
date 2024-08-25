e24ccfc420d6c5f883b6d8f7dc2e3a7e04d13cb7
David <nevermore6264@example.com>
2024-08-25 21:06:55 +0700

    update

--- crawler/crawler_data.js
+++ crawler/crawler_data.js
@@ -34,17 +34,24 @@ const { stringify } = require("csv-stringify");
         const contentElements = node.querySelectorAll("p");
         const content = Array.from(contentElements)
           .map((p) => p.innerText.trim()) // Lấy nội dung từng thẻ <p> và loại bỏ khoảng trắng đầu/cuối.
-          .join(" "); // Nối tất cả nội dung lại thành một chuỗi.
+          .join("\r\n"); // Nối tất cả nội dung lại thành một chuỗi.
 
         // Giả định thông tin bổ sung
-        const publishedAt = new Date(); // Giả định ngày xuất bản là hiện tại
+        const publishedAt = new Date().toISOString(); // Chuyển ngày xuất bản thành chuỗi định dạng ISO
         const status = "Published"; // Giả định trạng thái là "Published"
         const authorId = 1; // Giả định ID tác giả là 1 (bạn cần điều chỉnh theo nhu cầu của bạn)
         const categoryId = 1; // Giả định ID thể loại là 1 (bạn cần điều chỉnh theo nhu cầu của bạn)
 
         // Chỉ đẩy vào mảng nếu tiêu đề và liên kết không null.
         if (title && link) {
-          storyList.push({ title, content, publishedAt, status, authorId, categoryId });
+          storyList.push({
+            title,
+            content,
+            publishedAt,
+            status,
+            authorId,
+            categoryId,
+          });
         }
       });
 
@@ -68,7 +75,14 @@ async function saveDataToCSV(data, filename) {
   const writetabStream = fs.createWriteStream(filename);
 
   // Các cột (tiêu đề) trong file CSV.
-  const columns = ["title", "content", "publishedAt", "status", "authorId", "categoryId"];
+  const columns = [
+    "title",
+    "content",
+    "publishedAt",
+    "status",
+    "authorId",
+    "categoryId",
+  ];
 
   // Tạo một đối tượng stringifier để chuyển đổi dữ liệu thành CSV.
   const stringifier = stringify({
