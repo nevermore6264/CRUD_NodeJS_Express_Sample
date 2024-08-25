a8c1c0aae3bb44b373143ae11472868c79e42518
David <nevermore6264@example.com>
2024-08-25 21:15:43 +0700

    update

--- crawler/schedule.js
+++ crawler/schedule.js
@@ -1,53 +1,53 @@
 const cron = require("node-cron");
 const fs = require("fs");
 const csv = require("csv-parser");
-const productService = require("../service/product.service");
+const storyService = require("../service/story.service"); // Sửa lại đường dẫn service
 
 // Lên lịch cho các nhiệm vụ được thực thi trên máy chủ.
 // Chạy nhiệm vụ mỗi 5 giây
 cron.schedule("*/5 * * * * *", function () {
   console.log("running a task every 5 seconds");
-  updateProducts();
+  updateStories();
 });
 
+async function updateStories() {
+  try {
+    // Xóa dữ liệu trong bảng 'stories'
+    await storyService.truncate(); // Gọi phương thức truncate trong storyService
 
-async function updateProducts() {
-    try {
-      // Xóa dữ liệu trong bảng 'products'
-      await productService.truncate();
-  
-      // Đọc file và thêm dữ liệu mới
-      await readFile();
-    } catch (error) {
-      console.error("Error in updateProducts:", error);
-    }
+    // Đọc file và thêm dữ liệu mới
+    await readFile();
+  } catch (error) {
+    console.error("Error in updateStories:", error);
   }
+}
 
 function readFile() {
-  fs.createReadStream("saved_from_db.csv")
+  fs.createReadStream("stories.csv")
     .pipe(
       csv({
         separator: "|", // Đặt delimiter là '|'
       })
     )
     .on("data", async (row) => {
-        // console.log(row)
       try {
-        // Tạo product mới từ hàng trong file CSV
-        const newProduct = {
-          name: row.name,
-          price: parseFloat(row.price),
-          description: row.description,
-          categoryId: parseInt(row.categoryId, 10),
+        // Tạo story mới từ hàng trong file CSV
+        const newStory = {
+          title: row.title,
+          content: row.content,
+          publishedAt: row.publishedAt ? new Date(row.publishedAt) : null, // Chuyển đổi sang Date nếu có
+          status: row.status, // Giả định file CSV có trường status
+          authorId: parseInt(row.authorId, 10), // Giả định file CSV có trường authorId
+          categoryId: parseInt(row.categoryId, 10), // Giả định file CSV có trường categoryId
         };
 
-        // Sử dụng service để tạo sản phẩm trong cơ sở dữ liệu
-        await productService.create(newProduct);
+        // Sử dụng service để tạo truyện trong cơ sở dữ liệu
+        await storyService.create(newStory);
         console.log(
-          `Product "${newProduct.name}" has been added to the database.`
+          `Story "${newStory.title}" has been added to the database.`
         );
       } catch (error) {
-        console.error("Error adding product to the database:", error);
+        console.error("Error adding story to the database:", error);
       }
     })
     .on("end", () => {
